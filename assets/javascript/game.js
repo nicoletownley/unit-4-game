b1ea6defe93d570ce62f084763071e36bc712ee5
Your Name <natownley@gmail.com>
2019-07-20 21:46:47 -0500

    crystal fit

---
+++ assets/javascript/game.js
@@ -0,0 +1,3 @@
+// thoughts as Im researching:  in Jquery, can we select all 4 at once
+// (cat, swan, horse, koala) that will allow 
+//random value to be assigned to each individual one of them at the same time (within function)
