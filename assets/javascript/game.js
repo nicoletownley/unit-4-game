be844daae10d1c2ae4f0a39b551be06cbab4cb60
Your Name <natownley@gmail.com>
2019-07-22 19:34:01 -0500

    test random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,20 @@
 // thoughts as Im researching:  in Jquery, can we select all 4 at once
 // (cat, swan, horse, koala) that will allow 
-//random value to be assigned to each individual one of them at the same time (within function)
+//random value to be assigned to each individual one of them at the same time (within function)v
+
+var yourScore = 0
+var gameScore = 0
+var wins = 0
+var losses = 0
+//get random number 
+gameScore = Math.floor(Math.random() * (102) ) + 19;
+$("#rnumber").html ('Game Score:' + gameScore)
+
+//for each crystal
+     $("koala"). on(click, function yourScor {
+        return Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
+      }
+     if (yourScore > 0)
+      }
+    }); 
+    }+      
