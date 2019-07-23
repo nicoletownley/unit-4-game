a5cee37a5de962f06ca23ee844d328d3038c829a
Your Name <natownley@gmail.com>
2019-07-22 23:25:32 -0500

    random working for score need to see if divs for crytal create and append to crystals while generating numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,17 @@
 // (cat, swan, horse, koala) that will allow 
 //random value to be assigned to each individual one of them at the same time (within function)v
 
-var score = 0
+var score;
 //get random number 
 score = Math.floor(Math.random() * 102) + 19;
-console.log(score);
 
-
-//for each crystal
-   
+for (var i = 0; i < 4, i++) {
+    var animal =Math.floor(Math.random() * 12) + 1;
+    //this will generate 4 divs with a "crystal" for classes to be further honed in css (or html)
+    //the crysal click is a class I create myrself. I need to find out if I need to do something else
+    //tomorrow, print to console.log to see if code is working
+    var crystal = $("<div>");
+        crystal.attr({"class": 'crystal',"crystal click": animal
+     });
+     $(".crystals").append(crystal)
+    //for each crystal
